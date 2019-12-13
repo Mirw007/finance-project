f998a49871265dd410422da69f4ac70fde6174c4
Mirwang007 <1505174595@qq.com>
2019-12-13 10:30:43 +0800

    1213

---
+++ src/main/java/com/qf/j1906/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.qf.j1906.service;
+
+
+import com.qf.j1906.po.MsgResult;
+import com.qf.j1906.po.User;
+
+public interface MemberService {
+//    register
+    public MsgResult register(String uName, String uTel, String uPassword, String verifyCode);
+//    login
+    public MsgResult login(String uName, String uPassword);
+    //findMemberByName
+    public MsgResult findMemberByName(String uName);
+    //   edit
+    public MsgResult editMember(User user);
+    //delete
+    public MsgResult delMember(String uName);
+    public MsgResult getUserByName(String uName);
+
+}
